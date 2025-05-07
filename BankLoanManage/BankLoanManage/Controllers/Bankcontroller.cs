38bd4043718ea977c38b7ad62bfd01ec516a6c58
Abhith B N <ABHITH.BN@cognizant.com>
2025-05-07 22:05:45 +0530

    first commit

---
+++ BankLoanManage/BankLoanManage/Controllers/Bankcontroller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankLoanManage.Controllers
+{
+    public class Bankcontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult CustomerDashBoard()
+        {
+            return View();
+        }
+    }
+}
---
+++ BankLoanManage/BankLoanManage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankLoanManage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
